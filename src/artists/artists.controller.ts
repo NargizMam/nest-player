2c29ca01fabd8bad3354d231702f0159a3e951fe
Nargiz Mamytova <mam.zigran@gmail.com>
2024-03-20 16:47:26 +0600

    Создание и удаление исполнителя.

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,15 +1,26 @@
-import { Body, Controller, Get, Param, Post, Req } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { Artist, ArtistDocument } from '../schemas/artist.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Request } from 'express';
-import { CreateArtistsDto } from "./create-artists.dto";
+import { CreateArtistsDto } from './create-artists.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+
 @Controller('artists')
 export class ArtistsController {
   constructor(
     @InjectModel(Artist.name)
     private artistModel: Model<ArtistDocument>,
   ) {}
+
   @Get()
   getAll() {
     return this.artistModel.find();
@@ -21,12 +32,26 @@ export class ArtistsController {
   }
 
   @Post()
-  async create(@Body() artistDto: CreateArtistsDto) {
+  @UseInterceptors(
+    FileInterceptor('image', { dest: './public/uploads/artists/' }),
+  )
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() artistDto: CreateArtistsDto,
+  ) {
     const newArtist = new this.artistModel({
       title: artistDto.title,
       description: artistDto.description,
       isPublished: artistDto.isPublished,
+      image: file ? '/uploads/artists/' + file.filename : null,
     });
     return newArtist.save();
+  }
+
+  @Delete(':id')
+  async deleteArtist(@Param('id') id: string) {
+    console.log(id);
+    await this.artistModel.findByIdAndDelete(id);
+    return 'Исполнитель успешно удален!';
   }
 }
--- src/artists/create-artists.dto.ts
+++ src/artists/create-artists.dto.ts
@@ -1,7 +1,5 @@
 export class CreateArtistsDto {
   title: string;
-  image: string | null;
   description?: string;
   isPublished?: boolean;
-  user: string;
 }
